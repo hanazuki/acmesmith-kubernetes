dc81cde3cdb7d6d57eb7cb63bf27e09e90a40f8a
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-03-19 21:03:06 +0000

    init

---
+++ lib/acmesmith/kubernetes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "kubernetes/version"
+
+module Acmesmith
+  module Kubernetes
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/acmesmith/kubernetes_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Acmesmith::Kubernetes do
+  it "has a version number" do
+    expect(Acmesmith::Kubernetes::VERSION).not_to be nil
+  end
+end
